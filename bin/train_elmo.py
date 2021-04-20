66ed6bfc501f0830abd4b2583f863a968d27aed3
César González <ceguel@gmail.com>
2021-04-20 17:55:32 +0200

    Factor out training options

--- bin/train_elmo.py
+++ bin/train_elmo.py
@@ -1,9 +1,8 @@
-
 import argparse
+import json
+from pathlib import Path
 
-import numpy as np
-
-from bilm.training import train, load_options_latest_checkpoint, load_vocab
+from bilm.training import train, load_vocab
 from bilm.data import BidirectionalLMDataset
 
 
@@ -12,63 +11,28 @@ def main(args):
     vocab = load_vocab(args.vocab_file, 50)
 
     # define the options
-    batch_size = 128  # batch size for each GPU
-    n_gpus = 3
-
-    # number of tokens in training data (this for 1B Word Benchmark)
-    n_train_tokens = 768648884
-
-    options = {
-     'bidirectional': True,
-
-     'char_cnn': {'activation': 'relu',
-      'embedding': {'dim': 16},
-      'filters': [[1, 32],
-       [2, 32],
-       [3, 64],
-       [4, 128],
-       [5, 256],
-       [6, 512],
-       [7, 1024]],
-      'max_characters_per_token': 50,
-      'n_characters': 261,
-      'n_highway': 2},
-    
-     'dropout': 0.1,
-    
-     'lstm': {
-      'cell_clip': 3,
-      'dim': 4096,
-      'n_layers': 2,
-      'proj_clip': 3,
-      'projection_dim': 512,
-      'use_skip_connections': True},
-    
-     'all_clip_norm_val': 10.0,
-    
-     'n_epochs': 10,
-     'n_train_tokens': n_train_tokens,
-     'batch_size': batch_size,
-     'n_tokens_vocab': vocab.size,
-     'unroll_steps': 20,
-     'n_negative_samples_batch': 8192,
-    }
+    with open(args.options_file) as options_file:
+        options = json.load(options_file)
+    options['n_tokens_vocab'] = vocab.size
 
     prefix = args.train_prefix
     data = BidirectionalLMDataset(prefix, vocab, test=False,
-                                      shuffle_on_load=True)
+                                  shuffle_on_load=True)
 
     tf_save_dir = args.save_dir
+    Path(tf_save_dir).mkdir(parents=True, exist_ok=True)
     tf_log_dir = args.save_dir
-    train(options, data, n_gpus, tf_save_dir, tf_log_dir)
+
+    train(options, data, args.n_gpus, tf_save_dir, tf_log_dir)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--save_dir', help='Location of checkpoint files')
-    parser.add_argument('--vocab_file', help='Vocabulary file')
-    parser.add_argument('--train_prefix', help='Prefix for train files')
+    parser = argparse.ArgumentParser(description='Train a model')
+    parser.add_argument('--save_dir', help='Location of checkpoint files', default='model')
+    parser.add_argument('--vocab_file', help='Vocabulary file', default='vocab.txt')
+    parser.add_argument('--train_prefix', help='Prefix for train files', default='data.txt')
+    parser.add_argument('--options_file', help='Options JSON file', default='options.json')
+    parser.add_argument('--n_gpus', help='Number of GPUs', type=int, default=1)
 
     args = parser.parse_args()
     main(args)
-
